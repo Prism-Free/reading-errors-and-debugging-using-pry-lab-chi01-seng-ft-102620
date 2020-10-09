6f525e922447a067f94490809edff4490b26a889
Marcia Ann Freeman <marciafreeman2011@gmail.com>
2020-10-09 21:59:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -14,10 +14,10 @@ end
 def selection(num)
   if num = 1
 
-     "YUM YUM MUNCH MUNCH MUNCH"
+    p "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
 
-     "HAM HAM HAM IN MY TUMMY"
+    p "HAM HAM HAM IN MY TUMMY"
   end
 end
 
